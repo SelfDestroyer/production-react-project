06211e1de18534023b648b9eefe1f74b2f602537
Vova Ivaniuk <ivaniuk@Vovas-MacBook-Pro.local>
2023-10-01 16:12:25 +0300

    test screenshot tests configuration

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -6,6 +6,12 @@ const AboutPage = (): JSX.Element => {
   return (
     <div>
       <h1>{t('About Page')}</h1>
+      <h1>{t('About Page')}</h1>
+      <h1>{t('About Page')}</h1>
+      <h1>{t('About Page')}</h1>
+      <h1>{t('About Page')}</h1>
+      <h1>{t('About Page')}</h1>
+      <h1>{t('About Page')}</h1>
     </div>
   )
 }
